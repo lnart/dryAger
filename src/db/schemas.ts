4b7b60d1f53f36c533cf2b0c2160c409fdf82f10
Lennart <115736797+lnart@users.noreply.github.com>
2024-04-17 12:48:56 +0200

    change database schema

--- src/db/schemas.ts
+++ src/db/schemas.ts
@@ -1,57 +1,38 @@
 import mongoose from "mongoose";
 
-export const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  dryAgerIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "DryAger" }],
-});
-
-export const User = mongoose.model("user", userSchema);
-export type UserType = typeof userSchema;
-
-export const dryAgerSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
-  name: { type: String, required: true },
-  model: String,
+const dryAgerSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
   status: {
     light: { type: String, enum: ["on", "off"], default: "off" },
     fan: { type: String, enum: ["on", "off"], default: "off" },
   },
-});
-
-export const DryAger = mongoose.model("dryAger", dryAgerSchema);
-
-export const recordSchema = new mongoose.Schema({
-  dryAgerId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "dryAger",
-    required: true,
+  user: {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: false, unique: true },
+    password: { type: String, required: true },
   },
-  date: { type: Date, default: Date.now },
-  humidity: { type: Number, required: true },
-  temperature: { type: Number, required: true },
-  fanActivity: { type: String, enum: ["on", "off"], required: true },
-  lightActivity: { type: String, enum: ["on", "off"], required: true },
-});
-
-recordSchema.index({ dryAgerId: 1, date: -1 });
-
-export const Record = mongoose.model("record", recordSchema);
-
-export const recipeSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true },
-  name: { type: String, required: true },
-  description: String,
-  notes: String,
-  dryAgerId: {
+  recipes: [
+    {
+      title: { type: String, required: true },
+      description: { type: String, required: false },
+      notes: { type: String, required: true },
+      startDate: { type: Date },
+      endDate: { type: Date },
+    },
+  ],
+  records: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "dryAger",
-    required: true,
+    ref: "Record",
   },
-  startDate: { type: Date, required: true },
-  endDate: { type: Date, required: true },
-  relatedRecords: [{ type: mongoose.Schema.Types.ObjectId, ref: "record" }],
+});
+export const dryAgerModel = mongoose.model("DryAger", dryAgerSchema);
+
+const recordSchema = new mongoose.Schema({
+  date: { type: Date, required: true },
+  humidity: { type: Number },
+  temperature: { type: Number },
+  fanActivity: { type: String, enum: ["on", "off"] },
+  lightActivity: { type: String, enum: ["on", "off"] },
 });
 
-export const Recipe = mongoose.model("recipe", recipeSchema);
+export const recordModel = mongoose.model("Record", recordSchema);
