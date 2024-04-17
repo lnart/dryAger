651e3aef586c93654ffa8542e0057155a1b0006f
Lennart <115736797+lnart@users.noreply.github.com>
2024-04-17 18:57:20 +0200

    adjust schemas

--- src/db/schemas.ts
+++ src/db/schemas.ts
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const dryAgerSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   status: {
     light: { type: String, enum: ["on", "off"], default: "off" },
     fan: { type: String, enum: ["on", "off"], default: "off" },
   },
   user: {
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: false, unique: true },
+    username: { type: String, required: true },
+    email: { type: String, required: false, unique: true, sparse: true },
     password: { type: String, required: true },
   },
   recipes: [
@@ -20,15 +20,12 @@ const dryAgerSchema = new mongoose.Schema({
       endDate: { type: Date },
     },
   ],
-  records: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Record",
-  },
 });
 export const dryAgerModel = mongoose.model("DryAger", dryAgerSchema);
 
 const recordSchema = new mongoose.Schema({
-  date: { type: Date, required: true },
+  dryAgerId: { type: Schema.ObjectId, ref: "DryAger" },
+  date: { type: Date, required: true, default: new Date() },
   humidity: { type: Number },
   temperature: { type: Number },
   fanActivity: { type: String, enum: ["on", "off"] },
